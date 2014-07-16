e183ea6f60cf883663d19b834e0d2f7d9c16ee61
Srinivas Gorur-Shandilya <code@srinivas.gs>
2014-07-16 12:32:26 -0400

    added logistic.m;

--- FindShortestDimension.m
+++ FindShortestDimension.m
@@ -1,19 +1,17 @@
-% mean2.m
-% mean2 differs from mean in two different ways: it ignores NaNs, and if provided a matrix, mean2 intelligently operates on the shortest dimension. 
-% usage: m = mean2(x);
+% FindShortestDimension.m
+% finds the shortest dimension in a multidimensional matrix x
+% usage: d = FindShortestDimension(x)
 % 
 % created by Srinivas Gorur-Shandilya at 10:20 , 09 April 2014. Contact me at http://srinivas.gs/contact/
 % 
 % This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. 
 % To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
-function m = mean2(x)
-if ~nargin
-	help mean2
+
+function d = FindShortestDimension(x)
+if ~nargin 
+	help FindShortestDimension
 	return
 else
-	if isvector(x)
-		m = mean(x(~isnan(x)));
-	else
-		m = mean(x,FindShortestDimension(x));
-	end
+	s = size(x);
+	[~,d] = min(s);
 end
--- logistic.m
+++ logistic.m
@@ -1,19 +1,20 @@
-% mean2.m
-% mean2 differs from mean in two different ways: it ignores NaNs, and if provided a matrix, mean2 intelligently operates on the shortest dimension. 
-% usage: m = mean2(x);
-% 
+% 3-parameter logistic function
+% usage: r = logistic(x,xdata)
+% where x(1) specifies the maximum
+% x(2) specifies the location of the inflection point
+% x(3) specifies the steepness 
 % created by Srinivas Gorur-Shandilya at 10:20 , 09 April 2014. Contact me at http://srinivas.gs/contact/
 % 
 % This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. 
 % To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
-function m = mean2(x)
-if ~nargin
-	help mean2
+function r = logistic(x,xdata)
+if ~nargin 
+	help logistic
 	return
-else
-	if isvector(x)
-		m = mean(x(~isnan(x)));
-	else
-		m = mean(x,FindShortestDimension(x));
-	end
 end
+A = x(1);
+b = x(2);
+k  =x(3);
+ee = b - k*xdata;
+r = A./(1 + exp(ee));
+	
