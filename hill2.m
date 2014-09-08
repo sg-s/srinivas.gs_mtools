6975fdc3e65347f3e21afb4e09a92d8f39acd5cf
Srinivas Gorur-Shandilya <code@srinivas.gs>
2014-09-08 18:06:02 -0400

    added ihill.m

--- hill2.m
+++ hill2.m
@@ -1,8 +1,8 @@
 % 2-parameter Hill function
 % usage: r = hill2(x,xdata)
 % where 
-% x(2) specifies the location of the inflection point
-% x(3) specifies the steepness 
+% x(1) specifies the location of the inflection point
+% x(2) specifies the steepness 
 % the maximum (scaling) is automatically set to the maximum of the input
 % created by Srinivas Gorur-Shandilya at 10:20 , 09 April 2014. Contact me at http://srinivas.gs/contact/
 
--- ihill.m
+++ ihill.m
@@ -1,15 +1,14 @@
-% 3-parameter Hill function
-% usage: r = hill(x,xdata)
-% where x(1) specifies the maximum
-% x(2) specifies the location of the inflection point
-% x(3) specifies the steepness 
+% ihill.m
+% Inverse Hill Function
+% 
 % created by Srinivas Gorur-Shandilya at 10:20 , 09 April 2014. Contact me at http://srinivas.gs/contact/
 % 
 % This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. 
 % To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
-function r = hill(x,xdata)
+function [xdata] = ihill(x,ydata)
+
 if ~nargin 
-	help hill
+	help ihill
 	return
 end
 
@@ -17,8 +16,11 @@ A = x(1);
 k = x(2);
 n  =x(3);
 
-r = A*xdata.^n;
-r = r./(xdata.^n + k^n);
-	
-% when xdata is negative, return 0
-r(xdata<0) = 0;	
+
+xdata = ydata./(A - ydata);
+
+% handle pathalogical cases:
+xdata(xdata<0) = 0;
+
+xdata = xdata.^(1/n);
+xdata = xdata*k;
