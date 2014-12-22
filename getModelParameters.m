28116d8cdb7ff4d3e873a5549d0669198b775a14
Srinivas Gorur-Shandilya <code@srinivas.gs>
2014-12-22 15:58:10 -0500

    added getmodelparameters.m;

---
+++ getModelParameters.m
@@ -0,0 +1,39 @@
+% getModelParameters.m
+% usage:
+% p=getModelParameters(modelname);
+% returns a structure p that modelname needs
+% it works by reading modelname.m and figuring it out
+% works nice with Manipulate.m and FitModel2Data.m
+% assumes that modelname is of the form modelname(s,p)
+%
+% created by Srinivas Gorur-Shandilya at 3:20 , 22 December 2014. Contact me at http://srinivas.gs/contact/
+% 
+% This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. 
+% To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
+function p = getModelParameters(modelname)
+switch nargin
+	case 0
+		help getModelParameters
+	case 1
+		if ~ischar(modelname)
+			error('Argument must be a string')
+		end
+		if any(strfind(modelname,'.m'))
+		else
+			if ~exist(modelname)
+				error('Unknown function')
+			end
+			modelname = [modelname '.m'];
+		end
+end
+
+
+txt=fileread(modelname);
+a = strfind(txt,'p.');
+p = [];
+
+for i = 1:length(a)
+	this_snippet = txt(a(i)+2:length(txt));
+	f=this_snippet(1:(find(~isstrprop(this_snippet,'alphanum'),1,'first')-1));
+	eval(strcat('p.',f,'=0;'));
+end
