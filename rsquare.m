3108bc8e18c365a6aeaf6e92a7fa9479d66b33f4
sigbhu <github@srinivas.gs>
2015-02-04 13:31:53 -0500

    fix for #12

--- rsquare.m
+++ rsquare.m
@@ -1,14 +1,18 @@
 % rsquare.m
-% computes the coefficent of determination between two inputs
+% computes the coefficent of determination (http://en.wikipedia.org/wiki/Coefficient_of_determination) between two inputs
 % r = rsquare(x,y)
 % returns a scalar if x and y are vectors
 % 
-% if x and y are matrices, rsquare returns a matrix with dimensions equal to the width of x and y
+% r = rsquare(x)
+% if x is a matrix, rsquare returns a matrix r with dimensions equal to the width of x 
+% 
+% [r,s] = rsquare(x,y)
+% returns an additional output s, that contains the slopes of the lines fit to the two things being compared. 
 % created by Srinivas Gorur-Shandilya at 3:46 , 22 January 2015. Contact me at http://srinivas.gs/contact/
 % 
 % This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. 
 % To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
-function r = rsquare(x,y)
+function [r,s] = rsquare(x,y)
 switch nargin 
 	case 0
 		help rsquare
@@ -27,8 +31,9 @@ if isvector(x) && isvector(y)
 	x(deletethis) = [];
 	y(deletethis) = [];
 
-	[~, r]=fit(x,y,'Poly1');
+	[s, r]=fit(x,y,'Poly1');
 	r = r.rsquare;
+	s = s.p1;
 elseif ~isvector(x)
 	if size(x,1) > size(x,2)
 		x = x';
@@ -36,6 +41,7 @@ elseif ~isvector(x)
 	[a, adim] = width(x);
 
 	r = NaN(a);
+	s = NaN(a);
 	for i = 1:a-1
 		for j = i+1:a
 			if adim == 1
@@ -55,8 +61,9 @@ elseif ~isvector(x)
 			xx(deletethis) = [];
 			yy(deletethis) = [];
 
-			[~, temp]=fit(xx,yy,'Poly1');
+			[temp2, temp]=fit(xx,yy,'Poly1');
 			r(i,j) = temp.rsquare;
+			s(i,j) = temp2.p1;
 		end
 	end
 end
