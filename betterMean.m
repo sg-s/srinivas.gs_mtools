4cb531e4752461ea72f52922e4c5cb92300696a4
srinivas.gs <github@srinivas.gs>
2015-12-03 09:36:40 -0500

    wrote getAllSubFolders

--- betterMean.m
+++ betterMean.m
@@ -6,7 +6,7 @@
 % 
 % This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. 
 % To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
-function m = mean2(x)
+function m = betterMean(x)
 if ~nargin
 	help mean2
 	return
