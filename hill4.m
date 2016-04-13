5eddc9caaa0d3782cb538cf296c842082b92a67c
srinivas.gs <github@srinivas.gs>
2016-04-13 16:31:31 -0400

    added animatePhasePlot.m

--- hill4.m
+++ hill4.m
@@ -1,50 +1,23 @@
-% hill4.m
-% 4-parameter Hill function, with a X and a Y offset. the x-offset is automatically determined from the data, so be careful in using this in multiple datasets
+% hill2.m
+% 2-parameter Hill function
+% usage: r = hill2(xdata,K_D,n,x_offset)
 % 
-% usage:
-% r = hill4(xdata,A,k,n,y_offset)
-% or 
-% r = hill4(xdata,p)
-% where p is a parameter structure that contains all the parameters
-% 
-% hill5 can be fit to data using MATLAB's built in fit function, or using fitModel2Data
-%
 % created by Srinivas Gorur-Shandilya at 10:20 , 09 April 2014. Contact me at http://srinivas.gs/contact/
-% 
+%
 % This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. 
 % To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
-function r = hill4(xdata,A,k,n,y_offset)
-
-if ~nargin 
-	help hill4
+function r = hill4(xdata,A,K_D,n,x_offset)
+switch nargin
+case 0
+	help hill2
 	return
-elseif nargin == 2
-	assert(isstruct(A),'Second argument should be a structure')
-	p = A; clear A
-else
-	p.A = A;
-	p.k = k;
-	p.n = n;
-	p.y_offset = y_offset; 
+case 1
+	help hill2
+	error('Not enough inputs')
 end
 
-
-% parameters so that FitModel2Data can read this
-p.A;
-p.k;
-p.y_offset;
-p.n;
-
-% bounds
-lb.n = 1;
-lb.A = 0;
-
-
-if min(xdata) < 0
-	xdata = xdata - min(xdata);
-end
-
-r = p.A*xdata.^p.n;
-r = r./(xdata.^p.n + p.k^p.n);
-r = r + p.y_offset;
-
+xdata = xdata - x_offset;
+r = (xdata.^n)./(xdata.^n + K_D^n);
+r(xdata<eps) = 0;
+r = r*A;
+	
