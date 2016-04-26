74615ae439ee7122a30de2d4f03b908d28dcd1c7
srinivas.gs <github@srinivas.gs>
2016-04-26 16:48:54 -0400

    major reorganization

--- functions/gammaDist.m
+++ functions/gammaDist.m
@@ -7,9 +7,9 @@
 % 
 % This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. 
 % To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/. 
-function g = GammaDist(t,p)
+function g = gammaDist(t,p)
 if ~nargin 
-	help GammaDist
+	help gammaDist
 	return
 end
 g = (t.^(p.k-1)).*(exp(-t/p.theta));
