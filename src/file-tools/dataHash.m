6c8cbd7bba2c1099b9845a2a3e34f9d5dccabd3b
srinivas.gs <github@srinivas.gs>
2016-07-21 16:43:36 -0400

    disabled typecastx on PC

--- src/file-tools/dataHash.m
+++ src/file-tools/dataHash.m
@@ -134,6 +134,9 @@ persistent usetypecastx
 if isempty(usetypecastx)
    usetypecastx = ~isempty(which('typecastx'));  % Run the slow WHICH once only
 end
+if ispc
+   usetypecastx = false;
+end
 
 % Default options: -------------------------------------------------------------
 Method    = 'MD5';
