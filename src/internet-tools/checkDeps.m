56ba23173a54270c4b64880bf79a71c2c41c00c5
srinivas.gs <github@srinivas.gs>
2016-09-21 13:01:38 -0400

    wrote checkDeps

---
+++ src/internet-tools/checkDeps.m
@@ -0,0 +1,28 @@
+function [build_numbers] = checkDeps(toolboxes)
+
+build_numbers = NaN(length(toolboxes),1);
+
+try
+    if online
+        for i = 1:length(toolboxes)
+            try
+                [~,p]=searchPath(toolboxes{i});
+                build_numbers(i) = str2double(fileread([p oss 'build_number']));
+                v = checkForNewestVersionOnGitHub(['/sg-s/' toolboxes{i}]);
+                if v > build_numbers(i)
+                    disp(['A new version of ' toolboxes{i} ' is available: build' oval(v)])
+                    disp(['You have build ' oval(build_numbers(i))])
+                    disp(['Update using "install -f sg-s/' toolboxes{i} '"'])
+                else
+                    disp(['You have the latest version of ' toolboxes{i} ' -- build ' oval(build_numbers(i))])
+                end
+            catch
+                error('Missing toolboxes? Try updating and re-installing all toolboxes.')
+            end
+        end
+    else
+        warning('Could not check for updates.')
+    end
+catch
+    warning('Could not check for updates.')
+end
