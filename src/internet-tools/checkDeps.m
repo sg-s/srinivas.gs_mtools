4ac327c71e71c7f98852920fbcce0b0990cb41eb
srinivas.gs <github@srinivas.gs>
2016-10-20 16:50:16 -0400

    fixed a bug in checkDeps that returned build_numbers even when toolbox was missing

--- src/internet-tools/checkDeps.m
+++ src/internet-tools/checkDeps.m
@@ -7,17 +7,21 @@ try
         for i = 1:length(toolboxes)
             try
                 [~,p]=searchPath(toolboxes{i});
-                build_numbers(i) = str2double(fileread([p oss 'build_number']));
-                v = checkForNewestVersionOnGitHub(['/sg-s/' toolboxes{i}]);
-                if v > build_numbers(i)
-                    disp(['A new version of ' toolboxes{i} ' is available: build' oval(v)])
-                    disp(['You have build ' oval(build_numbers(i))])
-
-                    update_string = ['install -f sg-s/' toolboxes{i}]; 
-                    disp_string = ['<a href="matlab:' update_string '">click here to update</a>'];
-                    disp(disp_string)
+                if isempty(p)
+                    disp([toolboxes{i} ' not installed'])
                 else
-                    disp(['You have the latest version of ' toolboxes{i} ' -- build ' oval(build_numbers(i))])
+                    build_numbers(i) = str2double(fileread([p oss 'build_number']));
+                    v = checkForNewestVersionOnGitHub(['/sg-s/' toolboxes{i}]);
+                    if v > build_numbers(i)
+                        disp(['A new version of ' toolboxes{i} ' is available: build' oval(v)])
+                        disp(['You have build ' oval(build_numbers(i))])
+
+                        update_string = ['install -f sg-s/' toolboxes{i}]; 
+                        disp_string = ['<a href="matlab:' update_string '">click here to update</a>'];
+                        disp(disp_string)
+                    else
+                        disp(['You have the latest version of ' toolboxes{i} ' -- build ' oval(build_numbers(i))])
+                    end
                 end
             catch
                 error('Missing toolboxes? Try updating and re-installing all toolboxes.')
