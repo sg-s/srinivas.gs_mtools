0fb5957b5911efaee95acd0710f9577bcb825f6a
srinivas.gs <github@srinivas.gs>
2017-02-28 17:03:55 -0500

    updated spear to also return p value

--- src/maths-tools/spear.m
+++ src/maths-tools/spear.m
@@ -1,6 +1,9 @@
-% Spearman correlation coefficent, using MATLAB's built in corr function
+% computes Spearman correlation between two vectors x and y
+% uses MATLAB's built in corr function
+% this wrapper makes it a little easier to use
 
-
-function rho = spear(x,y)
-
-rho = corr(x(:),y(:),'type','Spearman');
+function [rho,p] = spear(x,y)
+x = x(:);
+y = y(:);
+rm_this = isnan(x) | isnan(y);
+[rho,p] = corr(x(~rm_this),y(~rm_this),'type','Spearman');
