38ebe88024c2b8e3c3b626a7b346500dffcad74f
srinivas.gs <github@srinivas.gs>
2017-02-28 13:58:59 -0500

    updated plotPieceWiseLinear, now can accept an axes handle as the first argument

--- src/matlab-tools/plotPieceWiseLinear.m
+++ src/matlab-tools/plotPieceWiseLinear.m
@@ -11,42 +11,62 @@ function [handles, data] = plotPieceWiseLinear(A,B,varargin)
 handles = [];
 data = [];
 
-% defualts  
-nbins = 10;
-LineWidth = 2;
-LineStyle = '-';
-Marker = 'none';
-trim_end = false;
-make_plot = true;
-Color = [0 0 0];
-use_std = false;
-proportional_bins = true;
-show_error = true;
+% defaults  
+options.nbins = 10;
+options.LineWidth = 2;
+options.LineStyle = '-';
+options.Marker = 'none';
+options.trim_end = false;
+options.make_plot = true;
+options.Color = [0 0 0];
+options.use_std = false;
+options.proportional_bins = true;
+options.show_error = true;
+options.plot_here = [];
 
-if ~nargin
-    help plotPieceWiseLinear
+
+if nargout && ~nargin 
+    varargout{1} = options;
     return
-else
-    if iseven(nargin)
-        for ii = 1:2:length(varargin)-1
-            temp = varargin{ii};
-            if ischar(temp)
-                eval(strcat(temp,'=varargin{ii+1};'));
-            end
-        end
-    else
-        error('Inputs need to be name value pairs')
-    end
 end
 
+% first argument can be an axis handle
+if isa(A,'matlab.graphics.axis.Axes')
+    options.plot_here = A;
+    A = B;
+    B = varargin{1};
+    varargin(1) = [];
+end
+
+% validate and accept options
+if iseven(length(varargin))
+    for ii = 1:2:length(varargin)-1
+    temp = varargin{ii};
+    if ischar(temp)
+        if ~any(find(strcmp(temp,fieldnames(options))))
+            disp(['Unknown option: ' temp])
+            disp('The allowed options are:')
+            disp(fieldnames(options))
+            error('UNKNOWN OPTION')
+        else
+            options.(temp) = varargin{ii+1};
+        end
+    end
+end
+elseif isstruct(varargin{1})
+    % should be OK...
+    options = varargin{1};
+else
+    error('Inputs need to be name value pairs')
+end
 assert(length(A) == length(B),'Inputs should have equal lengths')
 
 % is it a matrix or a vector?
 if isvector(A)
-    if proportional_bins
-        [x,y,xe,ye] = constructPWL(A,B,nbins);
+    if options.proportional_bins
+        [x,y,xe,ye] = constructPWL(A,B,options);
     else
-        [x,y,xe,ye] = constructPWL_equal_bins(A,B,nbins);
+        [x,y,xe,ye] = constructPWL_equal_bins(A,B,options);
     end
 else
     % it's a matrix. make sure it's oriented properly
@@ -56,13 +76,13 @@ else
     if size(B,1) < size(B,1)
         B = B';
     end
-    x = NaN(nbins,width(A));
+    x = NaN(options.nbins,width(A));
     xe = x; y = x; ye = x;
     for i = 1:width(A)
-        [x(:,i),y(:,i),xe(:,i),ye(:,i)] = constructPWL(A(:,i),B(:,i),nbins);
+        [x(:,i),y(:,i),xe(:,i),ye(:,i)] = constructPWL(A(:,i),B(:,i),options);
     end
     all_x = [min(min(x)) max(max(x))];
-    all_x = all_x(1):(diff(all_x)/nbins):all_x(end);
+    all_x = all_x(1):(diff(all_x)/options.nbins):all_x(end);
     all_y = NaN(length(all_x),width(A));
     
     for i = 1:width(A)
@@ -78,7 +98,7 @@ else
 
 
     all_x = all_x(~rm_this); all_x = [all_x(1) all_x(end)];
-    all_x = all_x(1):(diff(all_x)/nbins):all_x(end);
+    all_x = all_x(1):(diff(all_x)/options.nbins):all_x(end);
     all_y = NaN(length(all_x),width(A));
     
     for i = 1:width(A)
@@ -96,23 +116,30 @@ else
 end
 
 
-if trim_end
+if options.trim_end
     x = x(2:end-1);
     y = y(2:end-1);
     ye = ye(2:end-1);
 end
 
-if make_plot
-    if show_error
-        if nbins < 20
-            handles = errorbar(x,y,ye,'Color',Color,'LineWidth',LineWidth,'Marker',Marker,'LineStyle',LineStyle);
+if options.make_plot
+    if isempty(options.plot_here)
+        options.plot_here = gca;
+    else
+        if ~isvalid(options.plot_here)
+            options.plot_here = gca;
+        end
+    end
+    if options.show_error
+        if options.nbins < 20
+            handles = errorbar(options.plot_here,x,y,ye,'Color',options.Color,'LineWidth',options.LineWidth,'Marker',options.Marker,'LineStyle',options.LineStyle);
         else
-            [temp1, temp2] = errorShade(x,y,ye,'Color',Color,'LineWidth',LineWidth,'Marker',Marker,'LineStyle',LineStyle);
+            [temp1, temp2] = errorShade(options.plot_here,x,y,ye,'Color',options.Color,'LineWidth',options.LineWidth,'Marker',options.Marker,'LineStyle',options.LineStyle);
             handles.line = temp1;
             handles.shade = temp2;
         end
     else
-        handles = plot(x,y,'Color',Color,'LineWidth',LineWidth,'Marker',Marker,'LineStyle',LineStyle);
+        handles = plot(options.plot_here,x,y,'Color',options.Color,'LineWidth',options.LineWidth,'Marker',options.Marker,'LineStyle',options.LineStyle);
     end
 end
 
@@ -122,16 +149,16 @@ data.y = y;
 data.ye = ye;
 data.xe = xe;
 
-    function [x,y,xe,ye] = constructPWL(a,b,nbins)
+    function [x,y,xe,ye] = constructPWL(a,b,options)
         % remove NaNs
         rm_this = isnan(a) | isnan(b);
         a(rm_this) = [];
         b(rm_this) = [];
 
         % label x axis by percentile
-        l = labelByPercentile(a,nbins);
+        l = labelByPercentile(a,options.nbins);
 
-        x = NaN(nbins,1);
+        x = NaN(options.nbins,1);
         y = x; ye = x; xe = x;
 
         for ci = 1:max(l)
@@ -139,7 +166,7 @@ data.xe = xe;
 
             y(ci) = mean(b(l==ci));
             ye(ci) = sem(b(l==ci));
-            if use_std
+            if options.use_std
                 xe(ci) = std(a(l==ci));
                 ye(ci) = std(b(l==ci));
             else
@@ -149,16 +176,16 @@ data.xe = xe;
         end
     end
 
-    function [x,y,xe,ye] = constructPWL_equal_bins(a,b,nbins)
+    function [x,y,xe,ye] = constructPWL_equal_bins(a,b,options)
         % remove NaNs
         rm_this = isnan(a) | isnan(b);
         a(rm_this) = [];
         b(rm_this) = [];
 
         % make equally spaced bins along the x-axis
-        bin_edges = linspace(nanmin(A), nanmax(A),nbins+1);
-        y = NaN(nbins,1);
-        x = NaN(nbins,1);
+        bin_edges = linspace(nanmin(A), nanmax(A),options.nbins+1);
+        y = NaN(options.nbins,1);
+        x = NaN(options.nbins,1);
         ye = y; xe = x;
 
         for ci = 1:length(x)
@@ -169,7 +196,7 @@ data.xe = xe;
             if ~isempty(this_b)
                 y(ci) = mean(this_b);
                 x(ci) = mean(this_a);
-                if use_std
+                if options.use_std
                     ye(ci) = std(this_b);
                     xe(ci) = std(this_a);
                 else
