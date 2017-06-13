000b11a7f7cbc55628a79a2e70208743a587e0b4
srinivas.gs <github@srinivas.gs>
2017-06-13 17:54:08 -0400

    multi-core t-SNE now supports perplexity and n_iter arguments

--- src/maths-tools/mctsne.m
+++ src/maths-tools/mctsne.m
@@ -6,16 +6,28 @@
 % This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. 
 % To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
 
-function R = mctsne(Vs)
+function R = mctsne(Vs,n_iter,perplexity)
+
+if nargin < 2
+	n_iter = 1000;
+	perplexity = 30;
+elseif nargin < 3
+	perplexity = 30;
+end
 
 save('Vs.mat','Vs','-v7.3')
 
-system([fileparts(which('mctsne')) oss 'mctsne.py'])
+perplexity = floor(perplexity);
+n_iter = floor(n_iter);
+assert(n_iter > 10,'n_iter too low')
+assert(perplexity > 2,'perplexity too low')
+
+system([fileparts(which('mctsne')) oss 'mctsne.py ' oval(perplexity) ' ' oval(n_iter)])
 
 % read the solution
 R = h5read('data.h5','/R');
 
 % clean up
-delete('data.h5')
-delete('Vs.mat')
+% delete('data.h5')
+% delete('Vs.mat')
 
