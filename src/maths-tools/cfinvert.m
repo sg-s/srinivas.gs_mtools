e8c8c9010255c738b656a580340a20f48af38f6c
srinivas.gs <github@srinivas.gs>
2017-07-05 17:30:46 -0400

    added some help text for cfinvert

--- src/maths-tools/cfinvert.m
+++ src/maths-tools/cfinvert.m
@@ -1,7 +1,15 @@
 % cfinvert.m
 % given a curve fit object cf, find x such that cf(x) = y
+% using linear interpolation
+% usage:
+% x = cfinvert(cf,y,x_range)
+% where
+% cf is a curve fit object
+% y is the value you want to invert
+% and x_range is a 2-element vector specifying the approximate 
+% range you think x is at
 
-function [x] = cfinvert(cf,y,x_range)
+function x = cfinvert(cf,y,x_range)
 
 assert(isa(cf,'cfit'),'First argument should be a curve fit object')
 
