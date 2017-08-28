1b6d5ddb6db30fbad50bb815d918af66745fa042
gitlab@srinivas.gs <code@srinivas.gs>
2017-08-28 16:46:42 -0400

    fixed a big in mctsne

--- src/maths-tools/mctsne.m
+++ src/maths-tools/mctsne.m
@@ -22,7 +22,10 @@ n_iter = floor(n_iter);
 assert(n_iter > 10,'n_iter too low')
 assert(perplexity > 2,'perplexity too low')
 
-system([fileparts(which('mctsne')) oss 'mctsne.py ' oval(perplexity) ' ' oval(n_iter)])
+p1 = ['"' fileparts(which('mctsne'))];
+eval_str =  [p1 oss 'mctsne.py" ' oval(perplexity) ' ' oval(n_iter)];
+
+system(eval_str)
 
 % read the solution
 R = h5read('data.h5','/R');
