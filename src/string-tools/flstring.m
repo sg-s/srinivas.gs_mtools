ab46956835e57a3ff3d4d47d92f82019e9e56c9c
gitlab@srinivas.gs <code@srinivas.gs>
2017-12-07 11:26:17 -0500

    fixed a bug in flstring

--- src/string-tools/flstring.m
+++ src/string-tools/flstring.m
@@ -2,15 +2,15 @@
 % fixed length string
 % forces a string to be a fixed length
 
-function f = flstring(s,str_len)
+function s = flstring(s,str_len)
 
 if length(s) < str_len
-	f = [s repmat(' ', 1, str_len - length(s))];
+	s = [s repmat(' ', 1, str_len - length(s))];
 	return
 
 end
 
 if length(s) > str_len
-	f = s(1:str_len);
+	s = s(1:str_len);
 	return
 end
