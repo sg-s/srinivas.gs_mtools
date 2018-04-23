8be6315c4df351283675229a09bf15077c13fcad
gitlab@srinivas.gs <code@srinivas.gs>
2018-04-23 17:35:16 -0400

    lineWrite hopefully works on windows now

--- src/file-tools/lineWrite.m
+++ src/file-tools/lineWrite.m
@@ -8,16 +8,18 @@ function lineWrite(file_name,lines)
 assert(ischar(file_name),'First argument should be a string')
 assert(iscell(lines),'2nd argument should be a cell array')
 
-fileID = fopen(file_name,'w');
+
 
 if ispc
 	
 	for i = 1:length(lines)
 		this_line = strrep(lines{i},'%','%%');
-		fprintf(fileID, [this_line '\r\n']);
+		%fprintf(fileID, [this_line '\r\n']);
+		system(['echo ' this_line ' >> ' file_name] )
 	end
 
 else
+	fileID = fopen(file_name,'w');
 	for i = 1:length(lines)
 		this_line = strrep(lines{i},'%','%%');
 		fprintf(fileID, [this_line '\n']);
