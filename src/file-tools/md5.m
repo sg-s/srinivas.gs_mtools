12117f60fdecbc46c8b207efa23f0bafededd44a
gitlab@srinivas.gs <code@srinivas.gs>
2018-04-24 14:20:48 -0400

    experimenting with moving away from dataHash

--- src/file-tools/md5.m
+++ src/file-tools/md5.m
@@ -1,17 +1,5 @@
 % md5.m
-% returns md5 hash for anything you throw at it
-% for files, md5 uses system md5 (which is 2x faster than the one in dataHash, but falls back to dataHash if it fails)
-function [hash] = md5(file)
-if exist(file,'file') == 2	
-	[status,hash] = system(['md5 "' file '"']);
-	if status == 0
-		hash = hash((strfind(hash,'='))+2:end-1);
-	else
-		Opt.Input = 'file';
-		hash = dataHash(file,Opt);
-	end
-	
-else
-	hash = dataHash(file);
-end
-	
+% returns the md5 hash for anything you throw at it
+
+function H = md5(thing)
+
