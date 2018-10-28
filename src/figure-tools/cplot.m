d6b73bbe17f3e5d912ce9af1f512d32c1875eb0e
Srinivas Gorur-Shandilya <code@srinivas.gs>
2018-10-28 16:10:27 -0400

    improvements to cplot

--- src/figure-tools/cplot.m
+++ src/figure-tools/cplot.m
@@ -25,13 +25,51 @@ X = X(:);
 Y = Y(:);
 Z = Z(:);
 
+varargin(1:3) = [];
+
+% options and defaults
+options.clim = [NaN NaN];
+
+if nargout && ~nargin 
+	varargout{1} = options;
+    return
+end
+
+% validate and accept options
+if iseven(length(varargin))
+	for ii = 1:2:length(varargin)-1
+	temp = varargin{ii};
+    if ischar(temp)
+    	if ~any(find(strcmp(temp,fieldnames(options))))
+    		disp(['Unknown option: ' temp])
+    		disp('The allowed options are:')
+    		disp(fieldnames(options))
+    		error('UNKNOWN OPTION')
+    	else
+    		options.(temp) = varargin{ii+1};
+    	end
+    end
+end
+elseif isstruct(varargin{1})
+	% should be OK...
+	options = varargin{1};
+else
+	error('Inputs need to be name value pairs')
+end
+
 assert(length(X) == length(Y),'Vectors not of equal length')
 assert(length(X) == length(Z),'Vectors not of equal length')
 
 % make color scheme
-C = Z - min(Z);
-C = C/max(C);
-C = floor(C*999 + 1);
+if isnan(options.clim(1))
+	C = Z - min(Z);
+	C = C/max(C);
+	C = floor(C*999 + 1);
+else
+	C = Z - options.clim(1);
+	C = C/options.clim(2);
+	C = floor(C*999 + 1);
+end
 
 c = parula(1e3);
 
