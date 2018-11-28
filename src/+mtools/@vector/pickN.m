8bb250e668ddd891546ff551dfc6ce12272d63f8
gitlab@srinivas.gs <code@srinivas.gs>
2018-11-28 10:56:46 -0500

    added pickN

---
+++ src/+mtools/@vector/pickN.m
@@ -0,0 +1,22 @@
+% mtools.vector.pickN
+% 
+% picks N elements from a vector
+% this is guaranteed to return N
+% elements, no longer how long or short the input vector is
+% 
+% if X is more than N elements long, it picks N uniformly
+% spaced points
+% if X is less than N elements long, it returns X and NaN pads it
+% 
+function Y = pickN(X, N)
+
+assert(isvector(X), 'First argument should be a vector')
+X = X(:);
+
+if length(X) > N
+	D = floor(length(X)/N);
+	Y = X(1:D:end);
+else
+	Y = NaN(N,1);
+	Y(1:length(X)) = X;
+end
---
+++ src/+mtools/@vector/vector.m
@@ -0,0 +1,9 @@
+classdef vector
+
+methods (Static)
+
+	Y = pickN(X, N);
+
+end
+
+end % classdef
