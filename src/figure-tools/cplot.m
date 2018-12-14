b052ae55541bf1f5ca90cb98da1b32ecc37f73a6
gitlab@srinivas.gs <code@srinivas.gs>
2018-12-14 16:48:49 -0500

    improvements to cplot

--- src/figure-tools/cplot.m
+++ src/figure-tools/cplot.m
@@ -35,6 +35,8 @@ varargin(1:3) = [];
 
 % options and defaults
 options.clim = [NaN NaN];
+options.use_scatter = true;
+options.n_colors = NaN;
 
 if nargout && ~nargin 
 	varargout{1} = options;
@@ -66,33 +68,66 @@ end
 assert(length(X) == length(Y),'Vectors not of equal length')
 assert(length(X) == length(Z),'Vectors not of equal length')
 
-% make color scheme
-if isnan(options.clim(1))
+
+if options.use_scatter
+	% make color scheme
+	if isnan(options.clim(1))
+		C = Z - min(Z);
+		C = C/max(C);
+		C = floor(C*999 + 1);
+	else
+		C = Z - options.clim(1);
+		C = C/options.clim(2);
+		C = floor(C*999 + 1);
+	end
+
+	c = parula(1e3);
+
+	scatter_handle = scatter(ax,X,Y,24,c(C,:),'filled');
+
+	scatter_handle.Marker = 's';
+
+	chandle = colorbar(ax);
+	caxis(ax,[min(Z) max(Z)])
+
+
+	if nargout == 0
+	elseif nargout == 1
+		varargout{1} = scatter_handle;
+	elseif nargout == 2
+		varargout{1} = scatter_handle;
+		varargout{2} = chandle;
+	else
+		error('Too many outputs requested')
+	end
+
+else
+	% discretize the colormap and make N different plots
+	% with those colors
+
+	assert(~isnan(options.n_colors),'n_colors must be specified')
+
+	% discretize colors
 	C = Z - min(Z);
 	C = C/max(C);
-	C = floor(C*999 + 1);
-else
-	C = Z - options.clim(1);
-	C = C/options.clim(2);
-	C = floor(C*999 + 1);
-end
+	C = floor(C*(options.n_colors-1))+1;
+	c = parula(options.n_colors);
 
-c = parula(1e3);
+	for i = options.n_colors:-1:1
+		plot_handles(i) = plot(X(C==i),Y(C==i),'.','Color',c(i,:));
+	end
 
-scatter_handle = scatter(ax,X,Y,24,c(C,:),'filled');
+	chandle = colorbar(ax);
+	caxis(ax,[min(Z) max(Z)])
 
-scatter_handle.Marker = 's';
+	if nargout == 0
+	elseif nargout == 1
+		varargout{1} = plot_handles;
+	elseif nargout == 2
+		varargout{1} = plot_handles;
+		varargout{2} = chandle;
+	else
+		error('Too many outputs requested')
+	end
 
-chandle = colorbar(ax);
-caxis(ax,[min(Z) max(Z)])
-
-
-if nargout == 0
-elseif nargout == 1
-	varargout{1} = scatter_handle;
-elseif nargout == 2
-	varargout{1} = scatter_handle;
-	varargout{2} = chandle;
-else
-	error('Too many outputs requested')
 end
