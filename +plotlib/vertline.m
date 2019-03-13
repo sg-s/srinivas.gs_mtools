454716c34992d9a51c6582903eec3c70bbf861b2
gitlab@srinivas.gs <code@srinivas.gs>
2019-03-13 12:01:52 -0400

    wrote horzline and co

---
+++ +plotlib/vertline.m
@@ -0,0 +1,15 @@
+% draws a vertical line on the specified axis
+function lh = vertline(varargin)
+
+if isa(varargin{1},'matlab.graphics.axis.Axes')
+	ax = varargin{1};
+	varargin(1) = [];
+else
+	ax = gca;
+end
+
+X = varargin{1};
+
+Y = ax.YLim;
+
+lh = plot([X,X],Y);
