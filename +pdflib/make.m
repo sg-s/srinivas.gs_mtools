6bd26d348431f47ee8a43286ff75caa5967886f5
Srinivas Gorur-Shandilya <code@srinivas.gs>
2019-03-31 14:11:27 -0400

    improvements to pdflib

--- +pdflib/make.m
+++ +pdflib/make.m
@@ -13,7 +13,7 @@
 % 
 % This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. 
 % To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
-function [] = makePDF(varargin)
+function makePDF(varargin)
 
 
 assert(~ispc,'makePDF cannot run on a Windows computer')
@@ -31,26 +31,7 @@ options.dirty = false;
 options.filename = '';
 
 % validate and accept options
-if iseven(length(varargin))
-	for ii = 1:2:length(varargin)-1
-	temp = varargin{ii};
-    if ischar(temp)
-    	if ~any(find(strcmp(temp,fieldnames(options))))
-    		disp(['Unknown option: ' temp])
-    		disp('The allowed options are:')
-    		disp(fieldnames(options))
-    		error('UNKNOWN OPTION')
-    	else
-    		options.(temp) = varargin{ii+1};
-    	end
-    end
-end
-elseif isstruct(varargin{1})
-	% should be OK...
-	options = varargin{1};
-else
-	error('Inputs need to be name value pairs')
-end
+options = corelib.parseNameValueArguments(options, varargin{:});
 
 if isempty(options.filename)
 	options.filename = findFileToPublish;
@@ -124,7 +105,7 @@ system(['pdflatex "' f '"']);
 % clean up
 cd(orig_dir)
 if ~options.dirty
-	cleanPublish;
+	pdflib.clean;
 end
 close all
 
