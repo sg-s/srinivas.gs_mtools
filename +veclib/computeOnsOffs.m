34edc86198ce737eee11d0b05bd3f93360227c7f
Alec Hoyland <alec.hoyland@posteo.net>
2019-07-09 16:04:08 -0400

    whitespace

--- +veclib/computeOnsOffs.m
+++ +veclib/computeOnsOffs.m
@@ -3,8 +3,8 @@
 % usage:
 %  [ons,offs] = computeOnsOffs(x)
 % created by Srinivas Gorur-Shandilya at 10:20 , 09 April 2014. Contact me at http://srinivas.gs/contact/
-% 
-% This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. 
+%
+% This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
 % To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
 function [ons,offs] = computeOnsOffs(x)
 if ~nargin
