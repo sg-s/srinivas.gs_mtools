1ea746805720ad9b281a7bcfa9f57b8031c5d413
gitlab@srinivas.gs <code@srinivas.gs>
2019-11-08 10:53:19 -0500

    small improvement to adaptive

--- @adaptive/findCategoryEdges.m
+++ @adaptive/findCategoryEdges.m
@@ -1,7 +1,11 @@
 % this function finds the *points* at the middle of 
-% every triangle that has non-identical nodes
+% every edge with non-identical nodes.
+% These co-ordinates are returned in BX and BY
+%
+% it also returns index vectors to map these points
+% onto the nodes of the edges they came from
 
-function [BX,BY] = findCategoryEdges(self, make_plot)
+function [BX,BY, Nodes] = findCategoryEdges(self, make_plot)
 
 if ~iscategorical(self.data.values)
 	error('Cannot compute boundaries when output is non-categorical')
@@ -22,6 +26,10 @@ DT = delaunayTriangulation(X,Y);
 E = edges(DT);
 BX = NaN(size(E,1),1);
 BY = BX;
+
+% stores logical array of which edge nodes go into which point
+Nodes = NaN(size(E,1),2);
+
 for i = 1:size(E,1)
 	if length(unique(self.data.values(E(i,:)))) == 1
 		continue
@@ -29,8 +37,17 @@ for i = 1:size(E,1)
 	BX(i) = mean(X(E(i,:)));
 	BY(i) = mean(Y(E(i,:)));
 
+	Nodes(i,:) = E(i,:);
+
 
 end
 
+
+rm_this = isnan(BX) | isnan(BY);
+BX(rm_this) = [];
+BY(rm_this) = [];
+Nodes(rm_this,:) = [];
+
 % denormalize
 [BX,BY] = self.denormalize(BX,BY);
+
