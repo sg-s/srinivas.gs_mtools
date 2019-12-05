73a08c0f429281935fe34628a0acbd93f5dcecfb
gitlab@srinivas.gs <code@srinivas.gs>
2019-12-05 15:38:42 -0500

    fixed a bug in ISIDistance

--- +neurolib/+internal/ISI_parallel2.m
+++ +neurolib/+internal/ISI_parallel2.m
@@ -1,4 +1,4 @@
-function D = ISI_parallel2(x, Y)
+function D = ISI_parallel2(x, Y, FuncHandle)
 
 
 N = size(Y,2);
@@ -6,7 +6,7 @@ N = size(Y,2);
 D = zeros(N,1);
 
 for i = 1:N
-	D(i) = neurolib.internal.ISIDistance(x,Y(:,i));
+	D(i) = FuncHandle(x,Y(:,i));
 end
 
 
