f85b337bdeacc66a0081f1ac5de125ba1464ec04
Srinivas Gorur-Shandilya <code@srinivas.gs>
2020-04-23 11:58:19 -0400

    usability improvements to dictionary class

--- dictionary.m
+++ dictionary.m
@@ -8,9 +8,9 @@
 % creates a dictionary with one key called 'light purple'
 % Note the space in the key
 %
-% This key can be used like in a normal structure:
+% This key can be used to retrieve the value:
 %
-% colors.('light purple')
+% colors('light purple')
 %
 % View all keys in the dictionary:
 % 
@@ -31,21 +31,20 @@ methods
 		if length(varargin) == 0
 			return
 		end
-		
-
-		if length(varargin) == 1
-			% we are asking for a value using a key
-			keyboard
-		end
+	
 
 		% check that we are getting an even number of inputs
 		assert(floor(length(varargin)/2)*2 == length(varargin),'Expected an even number of arguments')
 
 		for i = 1:2:length(varargin)
-			keyhash = ['h' hashlib.md5hash(varargin{i})];
+			key = varargin{i};
+			if iscategorical(key)
+				key = char(key);
+			end
+			keyhash = ['h' hashlib.md5hash(key)];
 			self.addprop(keyhash);
 			self.(keyhash) = varargin{i+1};
-			self.keys = [self.keys varargin{i}];
+			self.keys = [self.keys; varargin{i}];
 		end
 
 	end
@@ -53,9 +52,17 @@ methods
 
 	function value = subsref(self,key)
 
+		if iscell(key.subs)
+			key.subs = key.subs{1};
+		end
+
 		if strcmp(key.subs,'keys')
 			value = self.keys;
 			return
+		end
+
+		if iscategorical(key.subs)
+			key.subs = char(key.subs);
 		end
 
 		keyhash = ['h' hashlib.md5hash(key.subs)];
@@ -67,12 +74,14 @@ methods
 
 	function self = subsasgn(self,key, value)
 
-
+		if iscategorical(key.subs)
+			key.subs = char(key.subs);
+		end
 
 		keyhash = ['h' hashlib.md5hash(key.subs)];
 		if ~isprop(self,keyhash)
 			self.addprop(keyhash);
-			self.keys = [self.keys key.subs];
+			self.keys = [self.keys; key.subs];
 		end
 		self.(keyhash) = value;
 	end
