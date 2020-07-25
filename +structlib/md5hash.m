d36b65fc96c63ebe6133c7bb74caca7b7a417f7b
Srinivas Gorur-Shandilya <code@srinivas.gs>
2020-07-25 15:09:39 -0400

    fixed #107

--- +structlib/md5hash.m
+++ +structlib/md5hash.m
@@ -3,12 +3,22 @@
 %
 function H = md5hash(S)
 
+if isempty(S)
+	H = hashlib.md5hash(S);
+	return
+end
+
 assert(isstruct(S),'Input must be a structure')
 assert(isscalar(S),'Input must be a scalar structure')
 
 S = orderfields(S);
 
 fn = fieldnames(S);
+
+if length(fn) == 0
+	H = hashlib.md5hash([]);
+	return
+end
 
 for i = length(fn):-1:1
 	this_field = S.(fn{i});
@@ -22,7 +32,14 @@ for i = length(fn):-1:1
 		this_field = [this_field{:}];
 	end
 
-	H{i} = hashlib.md5hash(this_field);
+	if isstruct(this_field)
+		H{i} = structlib.md5hash(this_field);
+	else
+		H{i} = hashlib.md5hash(this_field);
+	end
+
+	
 end
+
 
 H = hashlib.md5hash([H{:}]);
