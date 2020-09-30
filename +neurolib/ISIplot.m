863c6df2b4cb6d9d993760d4704b997071efe122
Srinivas Gorur-Shandilya <code@srinivas.gs>
2020-09-30 10:52:39 -0400

    fixed a bug in ISIplot

--- +neurolib/ISIplot.m
+++ +neurolib/ISIplot.m
@@ -1,6 +1,7 @@
-% plots spiketimes vs. isis
+% makes a plot of spiketimes vs. ISIs
+% 
 function h = ISIplot(spiketimes)
 
+spiketimes = spiketimes(:);
 isis = [NaN; diff(spiketimes)];
-
 h = plot(spiketimes,isis,'.');
