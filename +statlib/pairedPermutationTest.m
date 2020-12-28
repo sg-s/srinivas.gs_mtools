d5835ada67873e4b9fcc181d355daf50c97b33e7
Srinivas Gorur-Shandilya <code@srinivas.gs>
2020-12-28 16:08:04 -0500

    paired permutation test can now make plots

--- +statlib/pairedPermutationTest.m
+++ +statlib/pairedPermutationTest.m
@@ -5,7 +5,7 @@
 % permutation test to test if the means of A and B are
 % significantly different, and what the effect size is
 
-function p = pairedPermutationTest(A,B,N, Display)
+function p = pairedPermutationTest(A,B,N, MakePlot)
 
 
 arguments
@@ -13,7 +13,7 @@ arguments
 	A (:,1) double
 	B (:,1) double
 	N (1,1) double = 10000
-	Display char = 'full'
+	MakePlot (1,1) logical = false
 end
 
 assert(length(A) == length(B),'A and B should be of equal lengths')
@@ -32,15 +32,59 @@ for i = 1:N
 	D_this = D;
 	
 	D_this(shuffle(:,i)) = -D_this(shuffle(:,i));
-	D_shuffled(i) = mean(D_this);
+	D_shuffled(i) = nanmean(D_this);
 
 end
 
 
-D = mean(D);
+D = nanmean(D);
 
 if D < 0
 	p = mean(D_shuffled<D);
 else
 	p = mean(D_shuffled>D);
 end
+
+
+if ~MakePlot
+	return
+end
+
+
+[K,XX] = ksdensity(D_shuffled,'BandWidth',[]);
+K(XX>max(D_shuffled) | XX<min(D_shuffled)) = [];
+XX(XX>max(D_shuffled) | XX<min(D_shuffled)) = [];
+
+
+h = area(XX,K);
+h.FaceColor = [.8 .8 .8];
+h.EdgeAlpha = 0;
+h.FaceAlpha = .5
+
+
+if D > 0
+
+	if D < XX(end)
+		K(XX<D) = [];
+		XX(XX<D) = [];
+		h = area(XX,K);
+		h.LineWidth = 2;
+		h.FaceAlpha = .5;
+	else
+		plot([D D],[0 max(K)],'LineWidth',2)
+	end
+
+else
+	if D > XX(1)
+		K(XX>D) = [];
+		XX(XX>D) = [];
+		h = area(XX,K);
+		h.LineWidth = 2;
+		h.FaceAlpha = .5;
+	else
+		plot([D D],[0 max(K)],'LineWidth',2)
+	end
+
+
+end
+
