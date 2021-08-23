2b4ebfdff215daed13497adadfdbc5c4ac4942b6
Srinivas Gorur-Shandilya <code@srinivas.gs>
2021-08-23 18:51:31 -0400

    used strings to improve filelib.getAll

--- src/+filelib/getAll.m
+++ src/+filelib/getAll.m
@@ -3,16 +3,20 @@
 % http://stackoverflow.com/questions/2652630/how-to-get-all-files-under-a-specific-directory-in-matlab
 % usage: fileList = getAllFiles(dirName)
 function fileList = getAll(dirName)
-if ~nargin 
-	help filelib.getAll
-	fileList = {};
-	return
+
+arguments
+	dirName (1,1) string = pwd
 end
+
+
 dirData = dir(dirName);      % Get the data for the current directory
+
+
 dirIndex = [dirData.isdir];  % Find the index for directories
-fileList = transpose({dirData(~dirIndex).name});
+fileList = string(transpose({dirData(~dirIndex).name}));
+
 if ~isempty(fileList)
-	fileList = cellfun(@(x) fullfile(dirName,x), fileList, 'UniformOutput',false);
+	fileList = strcat(dirName,fileList);
 end
 subDirs = {dirData(dirIndex).name};  % Get a list of the subdirectories
 validIndex = ~ismember(subDirs,{'.','..'});  % Find index of subdirectories
