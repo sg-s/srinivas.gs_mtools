605d78881462fbc2f1aaeeda7bb4c15e90493424
Srinivas Gorur-Shandilya <code@srinivas.gs>
2021-09-29 09:53:37 -0400

    minor improvements to axlib.equalize

--- src/+axlib/equalize.m
+++ src/+axlib/equalize.m
@@ -60,10 +60,10 @@ eq_x = false;
 eq_y = false;
 eq_z = false;
 for i = 1:nargin
-	if strcmp(varargin{i},'x')
+	if strcmp(varargin{i},'y')
 		eq_y = true;
 	end
-	if strcmp(varargin{i},'y')
+	if strcmp(varargin{i},'x')
 		eq_x = true;
 	end
 	if strcmp(varargin{i},'z')
@@ -71,7 +71,8 @@ for i = 1:nargin
 	end
 end
 
-if ~eq_x & ~eq_y & ~eq_z
+
+if ~eq_x && ~eq_y && ~eq_z
 	% equalize nothing doesn't make sense. so let's equalize everything
 	eq_x = true;
 	eq_y = true;
@@ -83,7 +84,7 @@ if eq_x
 	min_x = min(xlims(1,:));
 	max_x = max(xlims(2,:));
 
-	for i = 1:length(ax_handles)
+	for i = length(ax_handles):-1:1
 		ax_handles(i).XLim = [min_x max_x];
 	end
 end
@@ -93,7 +94,7 @@ if eq_y
 	min_y = min(ylims(1,:));
 	max_y = max(ylims(2,:));
 
-	for i = 1:length(ax_handles)
+	for i = length(ax_handles):-1:1
 		ax_handles(i).YLim = [min_y max_y];
 	end
 end
@@ -103,7 +104,7 @@ if eq_z
 	min_z = min(zlims(1,:));
 	max_z = max(zlims(2,:));
 
-	for i = 1:length(ax_handles)
+	for i = length(ax_handles):-1:1
 		ax_handles(i).ZLim = [min_z max_z];
 	end
 end
