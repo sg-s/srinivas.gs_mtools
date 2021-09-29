35ba5ab68a7ec2d142d795b54064b1fc5b3ba4f1
Srinivas Gorur-Shandilya <code@srinivas.gs>
2021-09-29 15:23:15 -0400

    fixed a bug in plotlib.drawDiag

--- src/+plotlib/drawDiag.m
+++ src/+plotlib/drawDiag.m
@@ -12,9 +12,14 @@ M = max([max(x) max(y)]);
 
 hold(ax,'on');
 
-X = logspace(log10(eps),log10(M),1e3);
+if x(1) > 0 && x(2) > 0
+	X = logspace(log10(eps),log10(M),1e3);
+else
+	X = linspace(min([x(1) y(1)]),(M),1e3);
+end
 
-if length(varargin) == 0
+
+if isempty(varargin)
 	ph = plot(ax,X,X,'k--');
 else
 	ph = plot(ax,X,X,varargin{:});
