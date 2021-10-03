f3c9c0b1ab57d550b12235edd041b0967fc7fecc
Srinivas Gorur-Shandilya <code@srinivas.gs>
2021-10-03 11:31:53 -0400

    bugfix for axlib.set

--- src/+axlib/set.m
+++ src/+axlib/set.m
@@ -12,13 +12,20 @@ arguments
 end
 
 
-if ishandle(Value)
-	for i = 1:numel(ax)
-		set(ax(i),Name,copy(Value))
-	end
+% we need this idiotic try catch block because it is possible
+% that Value is a handle that we want to replicate across axes
+% if we don't copy a handle object, we just move it from place to 
+% place. however, there is no copy function for doubles, 
+% the worst thing is that there is no way to tell a graphics handle
+% from a double. ishandle is useless because ishandle(1) will always
+% be true if there is a figure open
+% isgraphics is equally useless
 
-else
-	for i = 1:numel(ax)
+for i = 1:numel(ax)
+	try
+		set(ax(i),Name,copy(Value))
+	catch
 		set(ax(i),Name,Value)
 	end
 end
+
